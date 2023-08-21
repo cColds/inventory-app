9f1a3925860390a21cd56a1ae8c87d7d6f3bc781
cColds <103373668+cColds@users.noreply.github.com>
2023-08-21 18:35:12 -0500

    Allow _id no-underscore-dangle eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "prefer-arrow-callback": "off",
-    "func-names": "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
-  },
-  ignorePatterns: ["bin/www"],
+    extends: ["airbnb-base", "prettier"],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    rules: {
+        "prefer-arrow-callback": "off",
+        "func-names": "off",
+        "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+        "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    },
+    ignorePatterns: ["bin/www"],
 };
