82dfed5ceee74154828c9c80cab92d5ef0df7718
cColds <103373668+cColds@users.noreply.github.com>
2023-08-23 06:08:44 -0500

    Add create item post route

--- routes/item.js
+++ routes/item.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const ItemModel = require("../models/item");
 const CategoryModel = require("../models/category");
 
 const router = express.Router();
 
 router.get("/create-item", async (req, res) => {
     const categories = await CategoryModel.find();
-
     res.render("item-form", { title: "Create Item", categories });
+});
+
+router.post("/create-item", async (req, res) => {
+    const {
+        "item-name": name,
+        "item-description": description,
+        "item-category": category,
+    } = req.body;
+    const price = Number(req.body["item-price"]);
+    const stock = parseInt(req.body["item-stock"], 10);
+
+    const item = new ItemModel({ name, description, category, price, stock });
+
+    item.save();
+    res.redirect("/");
 });
 
 module.exports = router;
